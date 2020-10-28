062679181ec1979cd9c84c30be409b2f7f38492e
X-San <ytbaconboi95@gmail.com>
2020-10-28 15:05:10 +0000

    a

--- test/testmain.js
+++ test/testmain.js
@@ -1,1 +1,13 @@
 const Discord = require('./../src/index.js');
+const stdvoid = require('./thingies/stdvoid.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log("client ready");
+});
+
+client.on('message', (message) => {
+  
+});
+
+client.login(process.env.dtoken, () => stdvoid) //login and gobble up the debug stuff that floods the console
---
+++ test/thingies/stdvoid.js
@@ -0,0 +1,10 @@
+/**
+ * stdVoid, For all your callback data disposal needs.
+ * X is not responible for lost data lmao.
+ * @returns {null}
+ */
+function stdvoid() {
+  
+}
+
+module.exports = stdvoid
